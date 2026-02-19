773aa605482f28610cf71941a261c72ea6cd7675
shivam-attri-85 <shivamkumarattri85@gmail.com>
2026-02-19 15:51:44 +0530

    Dart ? usage

---
+++ DART/demo2.dart
@@ -0,0 +1,19 @@
+// int main(){
+//   int? x;  //using ? so that the assign of null values to this variable is allowed 
+
+//   print(x);
+//   print(x!);     //!x checkin that value of x is not null
+//    return 0;
+//}//it gives error Unhandled exception:
+//Null check operator used on a null value
+
+
+int main(){
+  int ? x=10;   //here ? means x can carry null values 
+  print(x!);
+  return 0;
+
+//By default in dart null values cannot carry by variable 
+}
+//? : use to check the value will not null
+
