74dc55e0157594fea2e68a50d05864246577dcf1
shivam-attri-85 <shivamkumarattri85@gmail.com>
2026-02-20 14:02:42 +0530

    Input and Datatypes in Dart

---
+++ DART/DataTypes.dart
@@ -0,0 +1,107 @@
+
+void main(){
+/*🔹 1. Numbers
+
+Used to store numeric values.
+
+👉 int
+
+Whole numbers.
+*/
+int age = 20;
+print("print int value : $age");
+/*
+👉 double
+
+Decimal numbers.
+*/
+double price = 99.99;
+print("Print double value:$price");
+price=100;
+print("Print int value by using double :$price");
+/*
+👉 num
+
+Can store both int and double.
+*/
+num value = 10;
+print("print int value 10 to num datatype: $value");
+value = 10.5;
+
+print("Printting value double by assigning $value");
+/*\
+we can easily store anything in num
+🔹 2. String
+
+Stores text.
+*/
+
+String name = "Shivam";
+String message = 'Hello';
+
+//👉 String interpolation:
+
+print("Hello $name");
+// 🔹 3. Boolean (bool)
+/*
+Stores true/false.
+
+// bool isLoggedIn = true;
+🔹 4. List (Array)
+
+Stores multiple values in order.
+
+List<int> numbers = [1, 2, 3, 4];
+List<String> names = ["Ram", "Shyam"];
+
+Access:
+
+print(numbers[0]); // 1
+🔹 5. Set
+
+Stores unique values (no duplicates).
+
+Set<int> uniqueNumbers = {1, 2, 3};
+🔹 6. Map
+
+Stores key-value pairs.
+
+Map<String, String> student = {
+  "name": "Shivam",
+  "city": "Delhi"
+};
+
+Access:
+
+print(student["name"]);
+🔹 7. Dynamic
+
+Can change type anytime (not recommended for strict coding).
+
+dynamic value = 10;
+value = "Hello";
+🔹 8. var (Type Inference)
+
+Dart automatically detects type.
+
+var name = "Shivam";  // String
+var age = 21;         // int
+
+⚠️ Once assigned, type cannot change.
+
+🔹 9. Object & Object?
+
+Base type of all Dart objects.
+
+Object obj = "Hello";
+*/
+
+}
+
+
+
+/*
+int double and number
+
+
+*/
---
+++ DART/input.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+// void main(){
+//   print("Enter you age; ");
+//   int age =int.parse(stdin.readLineSync()!);
+//   print("Your age is : $age");
+// }
+//square of number
+void main(){
+  print("Enter number");
+  int n=int.parse(stdin.readLineSync()!);
+  int t=n*n;
+  int n1=int.parse(stdin.readLineSync()!);
+  print("Square of $n is $t ");
+  print ("square of $n1 is ${n1*n1}");
+}
+
+
+
